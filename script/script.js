2407ddcec20003a7dcd899eab5e182efae3a4652
Cory Gilliam <imaaxa@gmail.com>
2019-09-13 18:13:01 -0400

    Added question object example.

--- script/script.js
+++ script/script.js
@@ -1,1 +1,29 @@
+/*
+============================================
+; Title: exercise-6.2.js
+; Author: Cory Gilliam, Loren Wetzel, Tyler Librandi
+; Date: 13 Sept 2019
+; Modified By:
+; Description: JavaScript Knockout Trivia Game
+;===========================================
+*/
+
+// Variables
+var questionPool = [
+  // Use this as the object model for all of the questions
+  // Remember: Last item in a list dose "not" get a ,
+  // I tend to use single quotes for strings,
+  //     so all single quotes in the string needs to be escaped with a \
+  {
+    question: 'When printed, which of these strings would output: Good morning!Isn’ t it a“ lovely” day?',
+    choices: [
+      'A.var sentence = "Good morning!Isn\'t it a"lovely" day?"',
+      'B.var sentence = "Good morning!Isn\'t it a\'lovely\' day?"',
+      'C.var sentence = \'Good morning!Isn\'t it a"lovely" day?',
+      'D.var sentence = \'Good morning!Isn\'t it a\'lovely\ day?'
+    ],
+    answer : 2 // Answer needs to be the array index
+  }
+];
+
 // Script Start
