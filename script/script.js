189a1e51501f18a9851ad7e293e4b58530ace63e
Loren Wetzel <l.wetzel900@gmail.com>
2019-09-15 11:00:00 -0500

    adding my questions to js file

--- script/script.js
+++ script/script.js
@@ -23,6 +23,82 @@ var questionPool = [
       'D.var sentence = \'Good morning!Isn\'t it a\'lovely\ day?'
     ],
     answer : 2 // Answer needs to be the array index
+  },
+  { 
+    question: "What is 'NaN'",
+    choices: [
+      "A.None at Noon",
+      "B.Neither animal Nor",
+      "C.Not a Neither",
+      "D.Not a Number"
+    ],
+    answer: 3
+  },
+  {
+    question:"JavaScript is an OOP language by default?",
+    choices:[
+      "A. True",
+      "B. False"
+    ],
+    answer: 1
+  },
+  { 
+    question:"What is a prototype?",
+    choices:[
+      "A. A reference to another object",
+      "B. The car from that car show",
+      "C. Prototype... I have heard that before somewhere",
+      "D. A new object that I just discovered"
+    ],
+    answer: 0
+  },
+  { 
+    question:"How do you create an object with constructors?",
+    choices:[
+      "A. Construct the object.",
+      "B. Objects need constructors?",
+      "C. Create a constructor function.",
+      "D. JavaScript does not use objects."
+    ],
+    answer: 2
+  },
+  { 
+    question:"What is a callback function?",
+    choices:[
+      "A. Something I wish that person I meet the other day had.",
+      "B. A function that is executed after another function has finished",
+      "C. A function that calls you back.",
+      "D. Callback? There is no such thing."
+    ],
+    answer: 1
+  },
+  { 
+    question:"What are the common uses of JavaScript?",
+    choices:[
+      "A. Web development",
+      "B. Web Applications",
+      "C. Web Servers",
+      "D. All of the Above"
+    ],
+    answer: 3
+  },
+  { 
+    question:"A separate file is needed for JavaScript while programming a web app.?",
+    choices:[
+      "A. True",
+      "B. False"
+    ],
+    answer: 1
+  },
+  {
+    question: "What is the benefit of including 'use strict' at the start of a JavaScript file?",
+    choices: [
+      "A. Voluntary way to enforce stricter parsing",
+      "B. Voluntary way to enforce stricter error handling",
+      "C. Generate errors or throw exceptions for silent code errors",
+      "D. All of the above"
+    ],
+    answer: 3
   }
 ];
 
